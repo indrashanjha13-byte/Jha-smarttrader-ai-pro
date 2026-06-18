147d16d844f518410394a83410d284056e76deb7
indrashanjha13-byte <indrashanjha13@gmail.com>
2026-06-18 13:54:18 +0530

    Create BaseBroker class with basic methods

---
+++ base_broker.py
@@ -0,0 +1,21 @@
+class BaseBroker:
+
+    def login(self):
+        pass
+
+    def buy(
+        self,
+        symbol,
+        qty
+    ):
+        pass
+
+    def sell(
+        self,
+        symbol,
+        qty
+    ):
+        pass
+
+    def get_positions(self):
+        pass
