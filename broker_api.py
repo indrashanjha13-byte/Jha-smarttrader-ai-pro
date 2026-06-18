c90b309948e6039e0304ff11ad56c56700b171db
indrashanjha13-byte <indrashanjha13@gmail.com>
2026-06-18 14:02:02 +0530

    Implement BrokerAPI with buy and sell order methods

---
+++ broker_api.py
@@ -0,0 +1,19 @@
+class BrokerAPI:
+
+    def place_buy_order(
+        self,
+        symbol,
+        qty
+    ):
+        print(
+            f"BUY ORDER -> {symbol}"
+        )
+
+    def place_sell_order(
+        self,
+        symbol,
+        qty
+    ):
+        print(
+            f"SELL ORDER -> {symbol}"
+        )
