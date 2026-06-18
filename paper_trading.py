00ba9f2f0222f3b1642be26860219faf9264130a
indrashanjha13-byte <indrashanjha13@gmail.com>
2026-06-18 13:27:44 +0530

    Create paper_trading.py

---
+++ paper_trading.py
@@ -0,0 +1,80 @@
+import csv
+from datetime import datetime
+
+class PaperTrader:
+
+    def __init__(self):
+        self.balance = 100000
+        self.position = None
+
+    def buy(self, symbol, price, qty):
+
+        cost = price * qty
+
+        if cost > self.balance:
+            print("Insufficient Balance")
+            return
+
+        self.balance -= cost
+
+        self.position = {
+            "symbol": symbol,
+            "entry": price,
+            "qty": qty
+        }
+
+        self.save_trade("BUY", symbol, price, qty)
+
+        print(f"BUY {symbol} @ {price}")
+
+    def sell(self, price):
+
+        if not self.position:
+            return
+
+        pnl = (
+            (price - self.position["entry"])
+            * self.position["qty"]
+        )
+
+        self.balance += (
+            price * self.position["qty"]
+        )
+
+        self.save_trade(
+            "SELL",
+            self.position["symbol"],
+            price,
+            self.position["qty"]
+        )
+
+        print(f"PNL = {pnl}")
+
+        self.position = None
+
+    def save_trade(self, side, symbol, price, qty):
+
+        with open(
+            "trade_history.csv",
+            "a",
+            newline=""
+        ) as f:
+
+            writer = csv.writer(f)
+
+            writer.writerow([
+                datetime.now(),
+                side,
+                symbol,
+                price,
+                qty
+            ])
+def check_exit(entry, current):
+
+    if current <= entry - 20:
+        return "STOPLOSS"
+
+    if current >= entry + 40:
+        return "TARGET"
+
+    return None
