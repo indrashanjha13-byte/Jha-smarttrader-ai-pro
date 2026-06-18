8bbfc96404c6c859db8ecee8db1e11c463b70794
indrashanjha13-byte <indrashanjha13@gmail.com>
2026-06-18 13:31:09 +0530

    Create telegram_bot.py

---
+++ telegram_bot.py
@@ -0,0 +1,20 @@
+import requests
+
+def send_alert(message):
+
+    token = "8778860843:AAHd06Hpdx5K5wy9ay0mvH-n1umvBesl8W8"
+
+    chat_id = "2019899357"
+
+    url = (
+        f"https://api.telegram.org/bot"
+        f"{token}/sendMessage"
+    )
+
+    requests.post(
+        url,
+        data={
+            "chat_id": chat_id,
+            "text": message
+        }
+    )
