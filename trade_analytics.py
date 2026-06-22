4e88b680c15fa0d09a0a89ae6658515583fb43d0
indrashanjha13-byte <indrashanjha13@gmail.com>
2026-06-22 08:17:30 +0530

    Add win_rate to trade analytics return values

--- trade_analytics.py
+++ trade_analytics.py
@@ -18,12 +18,15 @@ def get_trade_stats():
         return {
             "total": total_trades,
             "buy": buy_trades,
-            "sell": sell_trades
+            "sell": sell_trades,
+            "win_rate": win_rate
         }
 
     except:
         return {
             "total": 0,
             "buy": 0,
-            "sell": 0
+            "sell": 0,
+            "win_rate": 0
+
         }
