0b0afb140c4cd9936e85c8cc2960bb7fe2cb67d8
indrashanjha13-byte <indrashanjha13@gmail.com>
2026-06-22 23:36:42 +0530

    Replace bot token with placeholder
    Updated the Telegram bot token to a placeholder.

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,19 +2,20 @@ import requests
 
 def send_alert(message):
 
-    token = "8778860843:AAHd06Hpdx5K5wy9ay0mvH-n1umvBesl8W8"
-
+    token = "YOUR_NEW_TOKEN"
     chat_id = "2019899357"
 
     url = (
         f"https://api.telegram.org/bot"
         f"{token}/sendMessage"
     )
+
     response = requests.post(
-    url,
-    data={
-        "chat_id": chat_id,
-        "text": message
-    }
-)
+        url,
+        data={
+            "chat_id": chat_id,
+            "text": message
+        }
+    )
+
     print(response.text)
