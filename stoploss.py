0f3ad831d42329f263c48c8c657008d30783d795
indrashanjha13-byte <indrashanjha13@gmail.com>
2026-06-22 13:04:57 +0530

    Added Target and Auto Exit Logic

--- stoploss.py
+++ stoploss.py
@@ -4,3 +4,34 @@ def stoploss(
 ):
 
     return entry - (2 * atr)
+def target(
+    entry,
+    atr
+):
+
+    return entry + (4 * atr)
+
+
+def check_exit(
+    entry,
+    current_price,
+    atr
+):
+
+    sl = stoploss(
+        entry,
+        atr
+    )
+
+    tgt = target(
+        entry,
+        atr
+    )
+
+    if current_price <= sl:
+        return "STOPLOSS"
+
+    if current_price >= tgt:
+        return "TARGET"
+
+    return None
