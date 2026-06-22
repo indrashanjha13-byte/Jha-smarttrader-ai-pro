44fce42792caeaac0699e8c63dfbd3440538d066
indrashanjha13-byte <indrashanjha13@gmail.com>
2026-06-22 08:31:59 +0530

    Refactor create_features to stoploss calculation
    Refactor create_features function into stoploss function that calculates stop loss based on entry and atr.

--- stoploss.py
+++ stoploss.py
@@ -1,16 +1,6 @@
-def create_features(df):
+def stoploss(
+    entry,
+    atr
+):
 
-    df["EMA_DIFF"] = (
-        df["EMA20"] - df["EMA50"]
-    )
-
-    df["RSI_NORM"] = (
-        df["RSI"] / 100
-    )
-
-    df["VOL_RATIO"] = (
-        df["Volume"] /
-        df["Volume"].rolling(20).mean()
-    )
-
-    return df
+    return entry - (2 * atr)
