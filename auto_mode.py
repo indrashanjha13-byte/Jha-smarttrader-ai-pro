6372684bc6f5e606cdca5704cd64abd0fc87c1c9
indrashanjha13-byte <indrashanjha13@gmail.com>
2026-07-21 11:10:55 +0530

    Add scan interval configuration for auto trading

--- auto_mode.py
+++ auto_mode.py
@@ -1,5 +1,7 @@
 AUTO_TRADING = False
 
+SCAN_INTERVAL = 15   # seconds
+
 def enable_auto():
     global AUTO_TRADING
     AUTO_TRADING = True
@@ -10,3 +12,6 @@ def disable_auto():
 
 def is_enabled():
     return AUTO_TRADING
+
+def get_scan_interval():
+    return SCAN_INTERVAL
